4430f117d2d6bb76d09c85380e3538fea9f873ca
swutask <swutask@gmail.com>
2026-01-06 16:22:13 +0530

    fix:bg

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import { HeroComponent } from "@/components";
 import { cn } from "@/lib";
 
-const getHomeStyles = () => cn("flex w-full flex-col items-center justify-between bg-white dark:bg-black sm:items-start");
+const getHomeStyles = () => cn("flex w-full flex-col items-center justify-between bg-white bg-black sm:items-start");
 
 export default function Home() {
   return (
